90428f31ff7c009180851ccff33d7f92967d5f32
Venhar Arifi <venhar.arifi84@gmail.com>
2025-11-05 11:46:19 +0100

    TEST

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -20,4 +20,12 @@
     <x-header />
     <main class="container mx-auto p-4 mt-4">{{ $slot }}</main>
   </body>
+  <body class="bg-gray-100">
+    <x-header />
+    <main class="container mx-auto p-4 mt-4">{{ $slot }}</main>
+  </body>
+  <body class="bg-gray-100">
+    <x-header />
+    <main class="container mx-auto p-4 mt-4">{{ $slot }}</main>
+  </body>
 </html>
