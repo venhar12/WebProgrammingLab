38c34ddc6b1f85c2f3118f58ab322a8a81066b81
Venhar Arifi <venhar.arifi84@gmail.com>
2025-11-05 11:39:46 +0100

    TITLE CHANGED

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>WEB PROGRAMING 131705</title>
+    <title>WEB PROGRAMING 131705-ID</title>
     <script src="https://cdn.tailwindcss.com"></script>
 
     <link
